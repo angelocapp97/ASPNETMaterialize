6ebb7a4c11b119b48f6dc46ffd61da9e879c0e4a
Angelo Cappelletti <angelocapp97@gmail.com>
2018-04-11 11:43:28 +0200

    Installed required packages

---
+++ VidlyMaterialize/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartupAttribute(typeof(VidlyMaterialize.Startup))]
+namespace VidlyMaterialize
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}
